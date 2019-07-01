91f15920efc8fd4f1cf241db363f22f7b59b5112
Nich Secord <nicholassecord@gmail.com>
2019-07-01 15:57:16 -0700

    major wireframing in place

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyBMe8wub75E2as0p_HYoABdsSwe9axl2as',
+    authDomain: 'rps-multiplayer-fc8e3.firebaseapp.com',
+    databaseURL: 'https://rps-multiplayer-fc8e3.firebaseio.com',
+    projectId: 'rps-multiplayer-fc8e3',
+    storageBucket: '',
+    messagingSenderId: '500300842060',
+    appId: '1:500300842060:web:1cee0ba601dd01e0',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
